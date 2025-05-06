7a29317bba3b5636c28808f6d8472fcba00671c4
Sabda <sabdaavicennaa@gmail.com>
2025-05-06 17:50:33 +0700

    fix image fetch2

--- MYBLOG-BE/index.js
+++ MYBLOG-BE/index.js
@@ -13,8 +13,6 @@ dotenv.config();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/uploads", express.static("uploads")); 
 
 app.use(router);
